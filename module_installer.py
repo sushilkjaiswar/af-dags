f3efb1b6fa825564c19ff7db80c6920611940370
Sushil Jaiswar <sushil_k_jaiswar@yahoo.com>
2024-11-08 23:18:45 +0530

    Delete module_installer.py

--- module_installer.py
+++
@@ -1,9 +0,0 @@
-def install_and_import():
-    import subprocess
-    subprocess.check_call(["pip", "install", "your-required-library"])
-
-with DAG("dynamic_installation", start_date=datetime(2024, 1, 1)) as dag:
-    run_task = PythonOperator(
-        task_id="install_library",
-        python_callable=install_and_import
-    )
