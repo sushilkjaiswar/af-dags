b3fca29b1a871e1b0c57c3470eda5366ce63bb9c
Sushil Jaiswar <sushil_k_jaiswar@yahoo.com>
2024-11-10 00:25:50 +0530

    Update twitter_etl_dag.py

--- twitter_etl_dag.py
+++ twitter_etl_dag.py
@@ -18,40 +18,40 @@ def install_and_import():
     return
     
     
-@task
-def get_twitter_data():
-    import pandas as pd
-    data = [[random.randint(0,10),random.randint(50,150),random.randint(1,110),random.randint(25,75),] for _ in range(100)]
-    df = pd.DataFrame(data, columns=["a", "b", "c", "d"])
-    return df
+# @task
+# def get_twitter_data():
+#     import pandas as pd
+#     data = [[random.randint(0,10),random.randint(50,150),random.randint(1,110),random.randint(25,75),] for _ in range(100)]
+#     df = pd.DataFrame(data, columns=["a", "b", "c", "d"])
+#     return df
 
 
-@task
-def dump_data_to_bucket(tweet_list: list):
-    import pandas as pd
-    from minio import Minio
-    from io import BytesIO
+# @task
+# def dump_data_to_bucket(tweet_list: list):
+#     import pandas as pd
+#     from minio import Minio
+#     from io import BytesIO
 
-    MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME")
-    MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
-    MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
+#     MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME")
+#     MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
+#     MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
 
-    df = pd.DataFrame(tweet_list)
-    csv = df.to_csv(index=False).encode("utf-8")
+#     df = pd.DataFrame(tweet_list)
+#     csv = df.to_csv(index=False).encode("utf-8")
 
-    client = Minio("http://s3.object.com", access_key=MINIO_ROOT_USER, secret_key=MINIO_ROOT_PASSWORD, secure=False)
+#     client = Minio("http://s3.object.com", access_key=MINIO_ROOT_USER, secret_key=MINIO_ROOT_PASSWORD, secure=False)
 
-    # Make MINIO_BUCKET_NAME if not exist.
-    found = client.bucket_exists(MINIO_BUCKET_NAME)
-    if not found:
-        client.make_bucket(MINIO_BUCKET_NAME)
-    else:
-        print(f"Bucket '{MINIO_BUCKET_NAME}' already exists!")
+#     # Make MINIO_BUCKET_NAME if not exist.
+#     found = client.bucket_exists(MINIO_BUCKET_NAME)
+#     if not found:
+#         client.make_bucket(MINIO_BUCKET_NAME)
+#     else:
+#         print(f"Bucket '{MINIO_BUCKET_NAME}' already exists!")
 
-    # Put csv data in the bucket
-    client.put_object(
-        "airflow-bucket", "twitter_elon_musk.csv", data=BytesIO(csv), length=len(csv), content_type="application/csv"
-    )
+#     # Put csv data in the bucket
+#     client.put_object(
+#         "airflow-bucket", "twitter_elon_musk.csv", data=BytesIO(csv), length=len(csv), content_type="application/csv"
+#     )
 
 
 @dag(
@@ -61,7 +61,8 @@ def dump_data_to_bucket(tweet_list: list):
     tags=["twitter", "etl"],
 )
 def twitter_etl():
-    dump_data_to_bucket(get_twitter_data(install_and_import()))
+    # dump_data_to_bucket(get_twitter_data(install_and_import()))
+    install_and_import()
 
 
 twitter_etl()
