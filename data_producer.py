93b0de7e5f2a1728244fb305a250edc2c6d07bea
Sushil Jaiswar <sushil_k_jaiswar@yahoo.com>
2024-11-10 15:10:29 +0530

    Update data_producer.py

--- data_producer.py
+++ data_producer.py
@@ -13,7 +13,7 @@ import os
 wildfires_dataset = Dataset("s3://s3.minio.com/data-warehouse/ml/")
 os.environ["HOURS_AGO"] = "1"
 
-def _transfer_from_api_to_s3(s3_conn_id, api_conn, endpoint, args, bucket, key):
+def _transfer_from_api_to_s3():
     data = [{"id":1,"first_name":"Rosco","last_name":"Reddyhoff","email":"rreddyhoff0@blogger.com","gender":"Male","ip_address":"9.46.93.93"},
             {"id":2,"first_name":"Kaye","last_name":"Barbary","email":"kbarbary1@seesaa.net","gender":"Female","ip_address":"57.90.84.94"},
             {"id":3,"first_name":"Hamlen","last_name":"Grigs","email":"hgrigs2@newyorker.com","gender":"Male","ip_address":"10.222.119.144"},
